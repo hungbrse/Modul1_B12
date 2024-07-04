2d45ff9805f32b717e72209c7e1a7172d4aa1170
hungdz2k1 <nguyenphuonghung1002@gmail.com>
2024-07-04 17:10:37 +0700

    update

---
+++ session12.js
@@ -0,0 +1,83 @@
+
+
+
+
+
+
+
+
+
+// let input =+(prompt("hehe"));
+
+
+// if (Number.isInteger(input) === true && input > 0 && input < 13) {
+
+//     if(input === 1 || input === 2 || input === 3) {
+//         alert("xuan")
+//     } else if(input === 4 || input ===  5 || input === 6) {
+//         alert("ha")
+    
+//     }  else if(input === 7 || input ===  8 || input === 9) {
+//         alert("thu")
+    
+//     } else {
+//         alert("dong")
+    
+//     }
+     
+// } else {
+//     alert("thang khong hop le")
+
+// }
+
+
+// bai 1
+
+// let input = +(prompt("Nhập 1 số"));
+
+
+
+
+// let sum = 0;
+
+
+
+// for (let index = 0; index <= input; index++) {
+
+//     // array.push(index);
+
+//     sum = sum + index
+// }
+
+// console.log(sum);
+
+
+// bai 2
+
+
+
+//  let input = +(prompt("Nhập 1 số"));
+
+//  let sum = 0;
+
+
+//  for (let index = 1; index <= input; index++) {
+  
+
+//  sum = sum + (index*index);
+
+    
+//  }
+
+//  console.log(sum)
+
+
+
+// bai 3
+
+
+  let input = +(prompt("Nhập 1 số"));
+
+
+  
+
